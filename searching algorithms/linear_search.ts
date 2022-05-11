0506f7dc6245848c93f15ad385e30809e0ba5768
Lucas Deschamps <lucas.catelli@b2wdigital.com>
2022-05-11 18:41:30 -0300

    linear search update

--- searching algorithms/linear_search.ts
+++ searching algorithms/linear_search.ts
@@ -1,11 +1,14 @@
-// linear search algorithm; just my little implementation of indexOf using TypeScript
+type SearchTypes = string | boolean | number;
 
-const myIndex = <T>(arr: T[], value: T): number => {
+const myIndex = <T extends SearchTypes>(arr: T[], value: T): number => {
   for (let i: number = 0; i < arr.length; i++) {
     if (arr[i] === value) return i;
   };
   return -1;
 };
 
-console.log(myIndex<number>([1, 2, 3, 4, 5], 5));
+
+console.log(myIndex<number>([5, 4, 3, 2, 1], 1));
 console.log(myIndex<string>(['a', 'b', 'c', 'd', 'e'], 'd'));
+console.log(myIndex([false, false, true, false, true], true));
+console.log(myIndex<number | string>([5, "4", "3", 2, 1], "4"));
